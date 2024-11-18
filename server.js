5ddc34552ceef184100ca62374e98b94e202a736
Mozart Gomes <17232048@esuda.edu.br>
2024-11-18 18:40:04 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -9,25 +9,25 @@ const bodyParser = require('body-parser');
 
 dotenv.config();
 
+// Conectar ao banco de dados
 connectDB();
 
 const app = express();
 
+// Middleware para permitir requisições de diferentes origens
 app.use(cors());
 
+// Middleware para ler o corpo das requisições como JSON
 app.use(express.json());
 app.use(bodyParser.json()); // Para ler o corpo das requisições em JSON
 
-app.use("/api/auth", authRoutes);
-app.use("/api/students", studentRoutes);
+// Definir as rotas da API
+app.use("/api/auth", authRoutes); // Rotas de autenticação (login, cadastro, redefinição de senha)
+app.use("/api/students", studentRoutes); // Rotas de estudantes
 
+// Definir a porta e iniciar o servidor
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-
 });
-
-
-
-
